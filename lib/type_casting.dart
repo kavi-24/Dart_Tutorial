6d56036c2ee638b20a0ed4f3ddbf39282b2fdc80
MasterKavi-24 <srikavipriyan3@gmail.com>
2023-09-01 12:00:39 +0530

    Create type_casting.dart

---
+++ lib/type_casting.dart
@@ -0,0 +1,29 @@
+/*
+Type casting:
+  Process of changing one data type into another
+*/
+
+import 'dart:io';
+
+void main() {
+  int n1, n2;
+  print("Enter n1: ");
+
+  // string to int
+  n1 = int.parse(stdin.readLineSync()!);
+  print("Enter n2: ");
+  n2 = int.parse(stdin.readLineSync()!);
+
+  print("n1 = $n1 ; n2 = $n2");
+
+  String s1 = "5.24";
+  // string to double
+  double d1 = double.parse(s1);
+  print(d1);
+
+  // double/int to string
+  String s2 = n2.toString();
+  String s3 = d1.toString();
+  print("$s2 $s3");
+
+}
